63cc935d041e7222b559801172fefc305fe78dd2
zhangchi <zhangchi@letsflytech.com>
2019-03-30 16:51:38 +0800

    pandas_test2 测试

--- libs/pandas_test.py
+++ libs/pandas_test.py
@@ -42,11 +42,41 @@ food_info = pd.read_csv("../data_set/food_info.csv")
 # Skipping the assignment.
 #zinc_copper = food_info[["Zinc_(mg)", "Copper_(mg)"]]
 # print(food_info.columns.tolist())
-col_names = food_info.columns.tolist()
-gram_columns = []
+# col_names = food_info.columns.tolist()
+# gram_columns = []
+#
+# for c in col_names:
+#     if c.endswith("(g)"):
+#         gram_columns.append(c)
+# gram_df = food_info[gram_columns]
+# print(gram_df.head(3))
 
-for c in col_names:
-    if c.endswith("(g)"):
-        gram_columns.append(c)
-gram_df = food_info[gram_columns]
-print(gram_df.head(3))
+# 对元素数据进行操作
+# div_1000 = food_info["Iron_(mg)"] / 1000
+# Adds 100 to each value in the column and returns a Series object.
+# add_100 = food_info["Iron_(mg)"] + 100
+# Subtracts 100 from each value in the column and returns a Series object.
+# sub_100 = food_info["Iron_(mg)"] - 100
+# Multiplies each value in the column by 2 and returns a Series object.
+# mult_2 = food_info["Iron_(mg)"]*2
+# It applies the arithmetic operator to the first value in both columns, the second value in both columns, and so on
+# water_energy = food_info["Water_(g)"] * food_info["Energ_Kcal"]
+# water_energy = food_info["Water_(g)"] * food_info["Energ_Kcal"]
+# iron_grams = food_info["Iron_(mg)"] / 1000
+# food_info["Iron_(g)"] = iron_grams
+
+
+max_calories = food_info["Energ_Kcal"].max()
+# Divide the values in "Energ_Kcal" by the largest value.
+normalized_calories = food_info["Energ_Kcal"] / max_calories
+normalized_protein = food_info["Protein_(g)"] / food_info["Protein_(g)"].max()
+normalized_fat = food_info["Lipid_Tot_(g)"] / food_info["Lipid_Tot_(g)"].max()
+food_info["Normalized_Protein"] = normalized_protein
+food_info["Normalized_Fat"] = normalized_fat
+
+#print food_info["Sodium_(mg)"]
+food_info.sort_values("Sodium_(mg)", inplace=True)
+print(food_info["Sodium_(mg)"])
+#Sorts by descending order, rather than ascending.
+food_info.sort_values("Sodium_(mg)", inplace=True, ascending=False)
+print(food_info["Sodium_(mg)"])
